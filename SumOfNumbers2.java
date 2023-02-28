9e6420210b2e18e25a35be56aab75bddf3d263a2
Panchaithi-Parmesh <126224095+Panchaithi-Parmesh@users.noreply.github.com>
2023-02-28 14:05:11 +0530

    third commit

--- SumOfNumbers2.java
+++ SumOfNumbers2.java
@@ -6,9 +6,9 @@ public static void main(String args[])
 int x, y, sum;  
 Scanner sc = new Scanner(System.in);  
 System.out.print("Enter the first number: ");  
-x=integer.parseInt(args[0]); 
+x=Integer.parseInt(args[0]); 
 System.out.print("Enter the second number: ");  
-y =integer.parseInt(args[1]); 
+y =Integer.parseInt(args[1]); 
 sum = sum(x, y);  
 System.out.println("The sum of two numbers x and y is: " + sum);  
 }  
